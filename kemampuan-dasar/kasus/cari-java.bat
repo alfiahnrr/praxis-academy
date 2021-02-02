550818642c8ac4a0688a9cc0b91be734e72a7189
Nur Rizqi Alfiah <67181894+alfiahnrr@users.noreply.github.com>
2021-02-03 00:40:26 +0700

    Update cari-java.bat

--- kemampuan-dasar/kasus/cari-java.bat
+++ kemampuan-dasar/kasus/cari-java.bat
@@ -1,18 +1,24 @@
-1. Buka Notepad.
-2. Tulis script dibawah ini.
-    param ( [string]$SourceExe, [string]$DestinationPath )
-    $WshShell = New-Object -comObject WScript.Shell
-    $Shortcut = $WshShell.CreateShortcut($SourceExe)
-    $Shortcut.TargetPath = $DestinationPath
-    $Shortcut.Save() 
-3. Save dengan nama yang mudah (karena nantinya akan digunakan pada perintah di PowerShell), sebut saja Set-Shortcut dan berikan akhiran .ps1.
-4. Pindahkan script ke direktori sistem (C:\Windows\System32). Hal ini agar kamu dapat mengeksekusi perintah nircmd langsung tanpa perlu berpindah directory PowerShell.
-5. Buka PowerShell as Administrator.
-6. (Opsional) Masukan perintah dibawah ini agar PowerShell dapat menjalankan script. Lalu ketikan A.
-7. Set-ExecutionPolicy RemoteSigned
-    Berikut format sintaks yang dimasukan untuk membuat shortcut.
-    Set-ShortCut [Tujuan shortcut] [Target file atau folder]
-    Misalnya jika ingin membuat shortcut terhadap aplikasi Calculator. Kamu bisa memasukan perintah berikut.
-    Set-Shortcut "C:\Users\kikiaa\Desktop\Kalkulator.lnk" "C:\Windows\System32\calc.exe"
-8. Done.
-9. Setelah proses selesai, maka akan terbuat sebuah shortcut baru sesuai dengan nama aplikasi yang kamu buat.
+import java.io.File;
+import java.io.IOException;
+ 
+public class MenampilkanFileDirektori {
+    
+    private static final String DIR = ("C:bahasajavacom");
+    
+    public static void main(String[] args) {
+       
+       File file = new File(DIR);
+        
+       File [] myFiles = file.listFiles();
+       System.out.println("$ cari-java.sh $HOME/src);
+       System.out.println("Ada file Java pada direktori  " + DIR + " ); 
+       //foreach loop
+        for(File files : myFiles){
+            try {
+                System.out.println(files.getCanonicalPath());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }             
+}
